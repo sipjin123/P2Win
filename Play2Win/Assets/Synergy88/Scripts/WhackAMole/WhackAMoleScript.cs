a15567d44b83aaa0a8713b71dd7fbb40b451dec5
Burlin S. Gauna <burlin_gauna@yahoo.com>
2015-10-30 11:16:17 +0800

    WhackAMole Mini Game

---
+++ Play2Win/Assets/Synergy88/Scripts/WhackAMole/WhackAMoleScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhackAMoleScript : MonoBehaviour {
+
+	[SerializeField] private GameObject Mole;
+
+	private const string MOLE_STATE = "isHiding";
+
+	private const int waitTime = 2;
+	private int stateChanged = 0;
+
+	private float stateDuration = 0.0f;
+	private bool isHiding = true;
+
+	private Animator MoleState;
+
+	void Start(){
+		MoleState = Mole.GetComponent<Animator> ();
+		StartCoroutine (WhackTheMole());
+		MoleState.SetBool (MOLE_STATE, isHiding);
+	}
+
+	IEnumerator WhackTheMole(){
+		yield return new WaitForSeconds (waitTime);
+
+		stateDuration = Random.Range (0.0f, 5.1f);
+		stateChanged = Random.Range (0, 101);
+		isHiding = stateChanged > 45 ? false : true;
+
+		if (!isHiding) {
+			MoleState.SetBool (MOLE_STATE, isHiding);
+		}
+		
+		yield return new WaitForSeconds (stateDuration);
+
+		if (!isHiding) {
+			isHiding = true;
+			MoleState.SetBool (MOLE_STATE, isHiding);
+		}
+		StartCoroutine (WhackTheMole());
+
+	}
+
+	void HideTheMole(){
+		if (!isHiding) {
+			isHiding = true;
+			MoleState.SetBool (MOLE_STATE, isHiding);
+		}
+	}
+}
