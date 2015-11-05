f0bc36fd565022283a7e984a150c2ebae6dc3665
Burlin S. Gauna <burlin_gauna@yahoo.com>
2015-11-05 12:42:55 +0800

    click button effect implementation(login, main_menu, and roulette)

---
+++ Play2Win/Assets/Synergy88/Scripts/Other/ClickEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickEffect : MonoBehaviour {
+
+	private tk2dSpriteAnimator myEffect;
+	// Use this for initialization
+
+	public static ClickEffect Instance { 
+		get; 
+		private set; 
+	}
+
+	void Awake () {
+		ClickEffect.Instance = this;
+		myEffect = this.GetComponent<tk2dSpriteAnimator>();
+	}
+	public void ButtonClickEffect(Vector3 p_pos){
+		this.gameObject.transform.position = p_pos;
+		if(myEffect != null)
+			myEffect.Play("ClickParticle");
+	}
+}
---
+++ Play2Win/Assets/Synergy88/Scripts/Other/TriggerClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerClick : MonoBehaviour {
+
+	void OnMouseDown(){
+		ClickEffect.Instance.ButtonClickEffect (this.gameObject.transform.position);
+	}
+}
