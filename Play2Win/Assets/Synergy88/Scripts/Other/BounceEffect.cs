73535e1990c349710e244fdc24b7c045fa3ff878
Burlin S. Gauna <burlin_gauna@yahoo.com>
2015-11-13 10:25:08 +0800

    main menu bounce effect

--- Play2Win/Assets/Synergy88/Scripts/Other/BounceEffect.cs
+++ Play2Win/Assets/Synergy88/Scripts/Other/BounceEffect.cs
@@ -8,37 +8,64 @@ public class BounceEffect : MonoBehaviour {
 	private Vector3 rightPos;
 	private Vector3 leftPos;
 	private bool isRight;
+	private bool isHold = false;
 
+	[SerializeField] private BounceEffect content;
 	[SerializeField] private GameObject[] characters;
 
 	void Awake(){
-		defaultXPos = this.transform.position.x;
-		originalPos1 = characters[0].transform.localPosition;
-		leftPos = characters [0].transform.localPosition + new Vector3 (-0.3f, 0.0f, 0.0f);
-		rightPos = characters [0].transform.localPosition + new Vector3 (0.3f, 0.0f, 0.0f);
+		defaultXPos = this.transform.localPosition.x;
+		originalPos1 = new Vector3(0.0f,0.4102091f,0.0f);
+		leftPos = characters [0].transform.localPosition + new Vector3 (-0.5f, 0.0f, 0.0f);
+		rightPos = characters [0].transform.localPosition + new Vector3 (0.5f, 0.0f, 0.0f);
+	}
+
+	IEnumerator moveCharacter(){
+		if (this.transform.localPosition.x > defaultXPos + 0.001f) {
+			for(int i = 0; i < characters.Length; i++){
+				characters [i].transform.localPosition = Vector3.Lerp (characters [i].transform.localPosition, leftPos, 0.1f);
+			}
+			yield return new WaitForSeconds (0.1f);
+		} 
+
+		else if (this.transform.localPosition.x < defaultXPos - 0.001f) {
+			for(int i = 0; i < characters.Length; i++){
+				characters [i].transform.localPosition = Vector3.Lerp (characters [i].transform.localPosition, rightPos, 0.1f);
+			}
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+
+	IEnumerator ResetPosition(){
+			for (int i = 0; i < characters.Length; i++) {
+				characters [i].transform.localPosition = Vector3.Lerp (characters [i].transform.localPosition, originalPos1, 0.2f);
+			}
+			yield return new WaitForSeconds (0.2f); 
 	}
 
 	void Update(){
-		if (this.transform.position.x > defaultXPos + 0.001f) {
-			characters[0].transform.localPosition = Vector3.Lerp(characters[0].transform.localPosition,leftPos,0.1f);
-			characters[1].transform.localPosition = Vector3.Lerp(characters[1].transform.localPosition,leftPos,0.1f);
-			characters[2].transform.localPosition = Vector3.Lerp(characters[2].transform.localPosition,leftPos,0.1f);
-			characters[3].transform.localPosition = Vector3.Lerp(characters[3].transform.localPosition,leftPos,0.1f);
+		if (this.gameObject.tag != "button") {
+			if (isHold) {
+				StartCoroutine (moveCharacter ());
+			} 
+			else
+				StartCoroutine (ResetPosition ());
+		}
+	}
 
-		}
-		else if (this.transform.position.x < defaultXPos - 0.001f) {
-			characters[0].transform.localPosition = Vector3.Lerp(characters[0].transform.localPosition,rightPos,0.1f);
-			characters[1].transform.localPosition = Vector3.Lerp(characters[1].transform.localPosition,rightPos,0.1f);
-			characters[2].transform.localPosition = Vector3.Lerp(characters[2].transform.localPosition,rightPos,0.1f);
-			characters[3].transform.localPosition = Vector3.Lerp(characters[3].transform.localPosition,rightPos,0.1f);
-			
-		}
-		else if(this.transform.position.x < defaultXPos + 0.002f && this.transform.position.x > defaultXPos - 0.002f){
-			characters[0].transform.localPosition = Vector3.Lerp(characters[0].transform.localPosition,originalPos1,0.3f);
-			characters[1].transform.localPosition = Vector3.Lerp(characters[1].transform.localPosition,originalPos1,0.3f);
-			characters[2].transform.localPosition = Vector3.Lerp(characters[2].transform.localPosition,originalPos1,0.3f);
-			characters[3].transform.localPosition = Vector3.Lerp(characters[3].transform.localPosition,originalPos1,0.3f);
-		}
+	public void setHold(bool p_hold){
+		isHold = p_hold;
+		defaultXPos = this.transform.localPosition.x;
+	}
+
+	void OnMouseDown(){
+		if (this.gameObject.tag == "button")
+			content.setHold (true);
+	}
+
+	void OnMouseUp(){
+		if(this.gameObject.tag == "button")
+			content.setHold (false);
 	}
 
 }
