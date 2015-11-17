624b002505532dd3f83cc8b866894d6a3c4ad67c
Burlin S. Gauna <burlin_gauna@yahoo.com>
2015-11-17 17:59:36 +0800

    Finalized Game Scene (still needs proper assets for game 2)

--- Play2Win/Assets/Synergy88/Scripts/SceneScripts/GameMenuScene.cs
+++ Play2Win/Assets/Synergy88/Scripts/SceneScripts/GameMenuScene.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.IO;
 
-public class GameMenuScene : AnimatedScene {
+public class GameMenuScene : AnimatedScene, ISignalListener {
 
     [SerializeField]
     private tk2dSpriteAnimator[] myAnimator;
@@ -13,18 +13,37 @@ public class GameMenuScene : AnimatedScene {
     [SerializeField]
     private Animator contentAnim;
 
+    [SerializeField]
+    private GameObject _level2LockedSprite;
+
+    [SerializeField]
+    private GameObject _level2AvailableSprite;
+
     void Start() {
         Initialize();
 
         ConcreteSignalParameters stateParam = new ConcreteSignalParameters();
         stateParam.AddParameter(SlotMachineScene.PARAM_LOCK, false);
         SignalManager.Instance.CallWithParam(SignalType.SLOT_MACHINE_STATE_CHANGED, stateParam);
+
+        SignalManager.Instance.Register(this, SignalType.LEVELED_UP);
+
+        UpdateGame2Availability();
+    }
+
+    void OnDestroy() {
+        SignalManager.Instance.Remove(this, SignalType.LEVELED_UP);
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Backspace)) {
             Exit();
         }
+    }
+
+    private void UpdateGame2Availability() {
+        _level2AvailableSprite.SetActive(PlayerDataManager.Instance.Level >= 2);
+        _level2LockedSprite.SetActive(PlayerDataManager.Instance.Level < 2);
     }
 
     public void LoadGame1() {
@@ -39,7 +58,14 @@ public class GameMenuScene : AnimatedScene {
     }
 
     public void LoadGame2() {
+        if (PlayerDataManager.Instance.Level < 2) {
+            return;
+        }
 
+        // Load Game 2 here
     }
 
+    public void Execute(SignalType type, ISignalParameters param) {
+        UpdateGame2Availability();
+    }
 }
