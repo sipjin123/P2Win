dde80eac442587a912f6f56f8351dd0f3d3b6935
Burlin S. Gauna <burlin_gauna@yahoo.com>
2015-11-26 15:23:25 +0800

    journey to the west minor bug fixes

--- Play2Win/Assets/Synergy88/Scripts/SlotMachine/SlotItem.cs
+++ Play2Win/Assets/Synergy88/Scripts/SlotMachine/SlotItem.cs
@@ -66,8 +66,8 @@ public class SlotItem : MonoBehaviour {
 		_itemChances.Add(SlotItemType.ITEM_7, 50f);
 		_itemChances.Add(SlotItemType.ITEM_8, 55f);
 		_itemChances.Add(SlotItemType.BONUS, 60f);
-//		_itemChances.Add(SlotItemType.SCATTER, 62.5f);
-//		_itemChances.Add(SlotItemType.WILD, 65f);
+		_itemChances.Add(SlotItemType.SCATTER, 62.5f);
+		_itemChances.Add(SlotItemType.WILD, 65f);
         //_itemChances.Add(SlotItemType.BOOSTER, 67.5f);
 	}
 
--- Play2Win/Assets/Synergy88/Scripts/SlotMachine/SlotMachineScene.cs
+++ Play2Win/Assets/Synergy88/Scripts/SlotMachine/SlotMachineScene.cs
@@ -412,6 +412,7 @@ public class SlotMachineScene : MonoBehaviour, ISignalListener {
 
 		yield return null;
 		if (!_spinning) {
+			spinButton.color = new Color(0.5f,0.5f,0.5f);
 			Spin();
 		}
 	}
