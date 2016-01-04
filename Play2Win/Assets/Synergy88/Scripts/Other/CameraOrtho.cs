fb22c09248e386a8ca0f1aed51868266b910bcd9
Burlin S. Gauna <burlin_gauna@yahoo.com>
2016-01-04 17:47:42 +0800

    playerprofile ui adjustment force scale

---
+++ Play2Win/Assets/Synergy88/Scripts/Other/CameraOrtho.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrtho : MonoBehaviour {
+
+	private const float myReso = 480.0f;
+	private const float minimumScale = 0.9f;
+	private float addScale = 0;
+
+	[SerializeField]
+	private Camera myCamera;
+
+	// Use this for initialization
+	void Awake () {
+		if (myCamera.aspect > 1.4f && myCamera.aspect < 1.7f) {
+			this.transform.localScale = new Vector3 (minimumScale + (myCamera.aspect - 1.4f), this.transform.localScale.y, this.transform.localScale.z);
+		} 
+		else if (myCamera.aspect > 1.7f) {
+			this.transform.localScale = new Vector3 (1.12f, this.transform.localScale.y, this.transform.localScale.z);
+		}
+		else {
+			this.transform.localScale = new Vector3(minimumScale, this.transform.localScale.y,this.transform.localScale.z);
+		}
+		Debug.Log (myCamera.aspect);
+	}
+}
